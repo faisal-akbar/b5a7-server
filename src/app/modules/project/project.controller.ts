699e195f0305c65d8f0dffd6bfc928cedcde8c9c
Faisal Akbar <faisalcep@yahoo.com>
2025-09-29 11:33:55 -0400

    project-controller

--- src/app/modules/project/project.controller.ts
+++ src/app/modules/project/project.controller.ts
@@ -1,12 +1,12 @@
 import { Project } from "@prisma/client";
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
 import { catchAsync } from "../../utils/catchAsync";
 import pick from "../../utils/pick";
 import { sendResponse } from "../../utils/sendResponse";
 import { projectFilterableFields } from "./project.constant";
 import { ProjectService } from "./project.service";
-import { JwtPayload } from "jsonwebtoken";
 
 const createProject = catchAsync(async (req: Request, res: Response) => {
   const payload: Project = {
@@ -53,7 +53,7 @@ const getSingleProject = catchAsync(async (req: Request, res: Response) => {
 const updateProject = catchAsync(async (req: Request, res: Response) => {
   const payload: Project = {
     ...req.body,
-    images: (req.files as Express.Multer.File[]).map((file) => file.path),
+    thumbnail: req.file?.path,
   };
   const result = await ProjectService.updateProject(
     parseInt(req.params.id),
