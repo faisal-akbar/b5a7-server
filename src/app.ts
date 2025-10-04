a8ed0f6f4fdf1fd2e2c73096aa8396979d6ecdf7
Faisal Akbar <faisalcep@yahoo.com>
2025-10-04 16:29:43 -0400

    app

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to Parcel Delivery System Backend",
+    message: "Welcome to Portfolio Backend",
   });
 });
 
